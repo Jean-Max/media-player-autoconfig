a86f97aacc3ba1ede0ebe77533b7b23ad409b49f
Jean-Max Marie-Sainte <jean-max-mariesainte@viseo.com>
2019-04-24 20:40:17 +0200

    Add missing annotation

--- src/main/java/com/viseo/formation/RunApplication.java
+++ src/main/java/com/viseo/formation/RunApplication.java
@@ -7,7 +7,7 @@ import com.viseo.formation.sound.MP3PlayerWithSetterMethod;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-
+@ContextConfiguration(classes = MediaPlayerConfig.class)
 public class RunApplication {
 
     public static void main(String[] args) {
